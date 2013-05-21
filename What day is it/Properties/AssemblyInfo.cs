3974f04833eee0798ed52a5ff87184d051afe3dd
IskhakovT <IskhakovT@gmail.com>
2013-05-21 16:16:28 +0000

    Add branch for localization

--- What day is it/Properties/AssemblyInfo.cs
+++ What day is it/Properties/AssemblyInfo.cs
@@ -46,4 +46,4 @@ using System.Resources;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.20.35")]
 [assembly: AssemblyFileVersion("1.20.35")]
-[assembly: NeutralResourcesLanguageAttribute("ru")]
+[assembly: NeutralResourcesLanguageAttribute("")]
