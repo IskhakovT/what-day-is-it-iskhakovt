9505a5ed632ffb4738c698799a993bf9e7a4a4ec
IskhakovT@gmail.com <IskhakovT@gmail.com@7f01a711-1a08-fca7-94cf-45bf5f0ebe9e>
2013-06-06 14:40:57 +0000

    1.20.36

--- trunk/What day is it/Properties/AssemblyInfo.cs
+++ trunk/What day is it/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using System.Resources;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.20.35")]
-[assembly: AssemblyFileVersion("1.20.35")]
+[assembly: AssemblyVersion("1.20.36")]
+[assembly: AssemblyFileVersion("1.20.36")]
 [assembly: NeutralResourcesLanguageAttribute("ru")]
